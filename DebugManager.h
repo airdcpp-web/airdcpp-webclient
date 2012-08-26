1db5209c521785ba582e61b20cc66b87dbc62c02
maksis <maksis@ec3e54a6-43b8-4a3e-9c49-2a733a73d9fe>
2012-08-26 11:49:21 +0000

    add a cool vertical filter in filelists, remove the detection option from the CDM debug frame
    git-svn-id: http://svn.airdcpp.net/airsvn/trunk@1012 ec3e54a6-43b8-4a3e-9c49-2a733a73d9fe

--- DebugManager.h
+++ DebugManager.h
@@ -16,10 +16,8 @@ public:
 template<int I>	struct X { enum { TYPE = I };  };
 
 	typedef X<0> DebugCommand;
-	typedef X<0> DebugDetection;	
 
-	virtual void on(DebugCommand, const string&) noexcept { }
-	virtual void on(DebugDetection, const string&, uint8_t, uint8_t, const string&) noexcept { }
+	virtual void on(DebugCommand, const string&, uint8_t, uint8_t, const string&) noexcept { }
 };
 
 class DebugManager : public Singleton<DebugManager>, public Speaker<DebugManagerListener>
@@ -29,9 +27,6 @@ class DebugManager : public Singleton<DebugManager>, public Speaker<DebugManager
 public:
 	void SendCommandMessage(const string& aMess, uint8_t aType, uint8_t aDirection, const string& aIP) {
 		fire(DebugManagerListener::DebugCommand(), aMess, aType, aDirection, aIP);
-	}
-	void SendDetectionMessage(const string& mess) {
-		fire(DebugManagerListener::DebugDetection(), mess);
 	}
 	~DebugManager() { };
 	enum Type {
