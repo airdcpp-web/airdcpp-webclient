a5bda32046d722c34e2d68ad4001e1407a05de47
night <->
2013-10-17 22:23:43 +0300

    -add script to get Git version info

--- StringDefs.cpp
+++ StringDefs.cpp
@@ -1813,7 +1813,6 @@ std::string dcpp::ResourceManager::strings[] = {
 "Application directories cannot be shared because they may contain passwords and other sensitive information", 
 "A completed item %1% has been removed", 
 "Override limiter", 
-"Clear", 
 "End time must be greater than the start time!", 
 };
 std::string dcpp::ResourceManager::names[] = {
@@ -3628,6 +3627,5 @@ std::string dcpp::ResourceManager::names[] = {
 "DontShareAppDirectory", 
 "CompleteItemXRemoved", 
 "OverrideLimiter", 
-"Clear", 
 "AsEndGreater", 
 };
