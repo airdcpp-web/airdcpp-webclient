20504e60728ebc07a5aff9825a2f0860e7eb50ff
maksis <maksis@adrenaline-network.com>
2014-01-31 19:51:58 +0200

    TBB fix

--- concurrency.h
+++ concurrency.h
@@ -26,6 +26,7 @@
 #include <tbb/task_group.h>
 #include <tbb/parallel_for_each.h>
 #include <tbb/concurrent_queue.h>
+#include "tbb/task_scheduler_init.h"
 
 namespace dcpp
 {
@@ -40,7 +41,7 @@ public:
 
 	~TaskScheduler() { }
 private:
-	task_scheduler_init init;
+	tbb::task_scheduler_init init;
 };
 
 }
