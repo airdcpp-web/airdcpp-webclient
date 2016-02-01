373af0d633ef26e6a31ebfec6de895b7360f4d71
maksis <maksis@adrenaline-network.com>
2016-02-01 16:31:48 +0200

    Allow installing non-git versions (fixes airdcpp-web/airdcpp-webclient#64)

--- generate_version.sh
+++ generate_version.sh
@@ -1,24 +1,33 @@
 #!/bin/sh
 
-if [ -z "$1" ]
+if [ -z "$3" ]
   then
-    echo "Usage: generate_version.sh <output file>"
+    echo "Usage: generate_version.sh <output file> <version number> <application name>"
 fi
+
+version=$2
+appName=$3
+versionDate=`date +%s`
+commitCount=0
 
 #check if we have a repository
 git ls-remote > /dev/null 2>&1
 if [ $? -ne 0 ];then
-   echo 'Not using a Git version'
-   exit 0
+  echo 'Not using a Git version'
+elif [ `git rev-parse --abbrev-ref HEAD` != "master" ];then
+	echo 'Git version detected'
+	version=`git describe --abbrev=4 --dirty=-d`
+	commitCount=`git rev-list HEAD --count`
+	versionDate=`git show --format=%at | head -n1`
 fi
 
 file=$1
 tmpFile="$file.tmp"
 
-echo "#define GIT_TAG \"`git describe --abbrev=4 --dirty=-d`\"" >> $tmpFile
-echo "#define GIT_COMMIT_COUNT `git rev-list HEAD --count`" >> $tmpFile
-echo "#define VERSION_DATE `git show --format=\"%at\" | head -n1`" >> $tmpFile
-echo "#define APPNAME_INC \"AirDC++w\"" >> $tmpFile
+echo "#define GIT_TAG \"$version\"" >> $tmpFile
+echo "#define GIT_COMMIT_COUNT $commitCount" >> $tmpFile
+echo "#define VERSION_DATE $versionDate" >> $tmpFile
+echo "#define APPNAME_INC \"$appName\"" >> $tmpFile
 
 if diff -q "$file" "$tmpFile" > /dev/null 2>&1; then
     : # files are the same
