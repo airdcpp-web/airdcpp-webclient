113ce5c5df27596186c397b987b0265f19fa82ca
maksis <maksis@adrenaline-network.com>
2017-06-04 11:49:50 +0300

    Squashed 'airdcpp-core/' changes from 846646a26..fda82ce74
    fda82ce74 Ignore messages regardless of the user levels (airdcpp-web/airdcpp-webclient#221)
    5dc304f05 Improve case conversion performance, cleanup
    
    git-subtree-dir: airdcpp-core
    git-subtree-split: fda82ce74670f4f42f714270d21ffa4b15e2e893

--- airdcpp/IgnoreManager.cpp
+++ airdcpp/IgnoreManager.cpp
@@ -151,13 +151,8 @@ bool IgnoreManager::checkIgnored(const OnlineUserPtr& aUser) noexcept {
 		return false;
 	}
 
-	// Increase the ignored message count
-	auto ignored = (aUser->getClient() && aUser->getClient()->isOp()) || !aUser->getIdentity().isOp() || aUser->getIdentity().isBot();
-	if (ignored) {
-		i->second++;
-	}
-
-	return ignored;
+	i->second++;
+	return true;
 }
 
 ActionHookRejectionPtr IgnoreManager::isIgnoredOrFiltered(const ChatMessagePtr& msg, const HookRejectionGetter& aRejectionGetter, bool PM) noexcept {
