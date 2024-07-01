5c1635cc40cabf7b4169abe48741682dc36bc86d
maksis <maksis@adrenaline-network.com>
2024-07-01 18:44:52 +0300

    Update scripts/run_demo.sh

--- scripts/run_demo.sh
+++ scripts/run_demo.sh
@@ -1,9 +1,5 @@
-sudo setcap cap_net_bind_service=+ep `readlink -f \`which airdcppd\``
-
 adchppd -d
-screen -dm -S democlient bash -c 'airdcppd -c=/home/airdcpp/airdcpp-demo/ --web-resources=/home/airdcpp/airdcpp-webui/demo/; exec bash'
+screen -dm -S democlient bash -c 'airdcppd -c=/home/airdcpp/airdcpp-demo/; exec bash'
 screen -dm -S shareclient bash -c 'airdcppd -c=/home/airdcpp/airdcpp-share/; exec bash'
 
-
-# screen -dm -S express bash -c 'sh /home/airdcpp/airdcpp-webui/scripts/start_demo.sh; exec bash'
 
