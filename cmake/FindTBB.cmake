01a33ed3c1296ccf26af96a523ec24fe5091be83
maksis <maksis@adrenaline-network.com>
2024-11-09 10:15:19 +0200

    Replace FindTBB.cmake
    The library didn't get linked previously
    
    https://github.com/airdcpp-web/airdcpp-webclient/issues/486

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -1,89 +1,98 @@
-# Copyright (c) 2020-2021 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-FileCopyrightInfo: Copyright © DUNE Project contributors, see file LICENSE.md in module root
+# SPDX-License-Identifier: LicenseRef-GPL-2.0-only-with-DUNE-exception
+
+#[=======================================================================[.rst:
+FindTBB
+-------
+
+Finds the Threading Building Blocks (TBB) library.
+
+This is a fallback implementation in case the TBB library does not provide
+itself a corresponding TBBConfig.cmake file.
+
+Imported Targets
+^^^^^^^^^^^^^^^^
+
+This module provides the following imported targets, if found:
+
+``TBB::tbb``
+  Imported library to link against if TBB should be used.
+
+Result Variables
+^^^^^^^^^^^^^^^^
+
+This will define the following variables:
+
+``TBB_FOUND``
+  True if the TBB library was found.
+
+Finding the TBB library
+^^^^^^^^^^^^^^^^^^^^^^^
+
+Two strategies are implemented for finding the TBB library:
+
+1. Searching for the TBB cmake config file, typically named
+   ``TBBConfig.cmake``. In recent TBB versions, this file can be
+   created using a script provided by TBB itself. Simply set the
+   variable ``TBB_DIR`` to the directory containing the config file
+   in order to find TBB.
+
+2. Using pkg-config to configure TBB. Therefore it is necessary
+   to find the ``tbb.pc`` file. Several distributions provide this file
+   directly. In order to point pkg-config to the location of that file,
+   simply set the environmental variable ``PKG_CONFIG_PATH`` to include
+   the directory containing the .pc file, or add this path to the
+   ``CMAKE_PREFIX_PATH``.
+
+#]=======================================================================]
+
+
+# text for feature summary
+include(FeatureSummary)
+set_package_properties("TBB" PROPERTIES
+  DESCRIPTION "Intel's Threading Building Blocks"
+  URL "https://github.com/oneapi-src/oneTBB"
+)
+
+# first, try to find TBBs cmake configuration
+find_package(TBB ${TBB_FIND_VERSION} QUIET CONFIG)
+if(TBB_FOUND AND TARGET TBB::tbb)
+  message(STATUS "Found TBB: using configuration from TBB_DIR=${TBB_DIR} (found version \"${TBB_VERSION}\")")
+  return()
+endif()
+
+# Add a backport of cmakes FindPkgConfig module
+if(${CMAKE_VERSION} VERSION_LESS "3.19.4")
+  list(INSERT CMAKE_MODULE_PATH 0 "${CMAKE_CURRENT_LIST_DIR}/FindPkgConfig")
+endif()
+
+# second, try to find TBBs pkg-config file
+find_package(PkgConfig)
+if(PkgConfig_FOUND)
+  if(TBB_FIND_VERSION)
+    pkg_check_modules(PkgConfigTBB tbb>=${TBB_FIND_VERSION} QUIET IMPORTED_TARGET GLOBAL)
+  else()
+    pkg_check_modules(PkgConfigTBB tbb QUIET IMPORTED_TARGET GLOBAL)
+  endif()
+endif()
+
+# check whether the static library was found
+if(PkgConfigTBB_STATIC_FOUND)
+  set(_tbb PkgConfigTBB_STATIC)
+else()
+  set(_tbb PkgConfigTBB)
+endif()
 
 include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args("TBB"
+  REQUIRED_VARS
+    ${_tbb}_LINK_LIBRARIES ${_tbb}_FOUND PkgConfig_FOUND
+  VERSION_VAR
+    ${_tbb}_VERSION
+  FAIL_MESSAGE "Could NOT find TBB (set TBB_DIR to path containing TBBConfig.cmake or set PKG_CONFIG_PATH to include the location of the tbb.pc file)"
+)
 
-# Firstly search for TBB in config mode (i.e. search for TBBConfig.cmake).
-find_package(TBB QUIET CONFIG)
-if (TBB_FOUND)
-    find_package_handle_standard_args(TBB CONFIG_MODE)
-    return()
+if(${_tbb}_FOUND AND NOT TARGET TBB::tbb)
+  add_library(TBB::tbb ALIAS PkgConfig::PkgConfigTBB)
 endif()
 
-if (NOT TBB_FIND_COMPONENTS)
-    set(TBB_FIND_COMPONENTS tbb tbbmalloc)
-    foreach (_tbb_component ${TBB_FIND_COMPONENTS})
-        set(TBB_FIND_REQUIRED_${_tbb_component} 1)
-    endforeach()
-endif()
-
-if (WIN32)
-    list(APPEND ADDITIONAL_LIB_DIRS ENV PATH ENV LIB)
-    list(APPEND ADDITIONAL_INCLUDE_DIRS ENV INCLUDE ENV CPATH)
-else()
-    list(APPEND ADDITIONAL_LIB_DIRS ENV LIBRARY_PATH ENV LD_LIBRARY_PATH ENV DYLD_LIBRARY_PATH)
-    list(APPEND ADDITIONAL_INCLUDE_DIRS ENV CPATH ENV C_INCLUDE_PATH ENV CPLUS_INCLUDE_PATH ENV INCLUDE_PATH)
-endif()
-
-find_path(_tbb_include_dir NAMES tbb/tbb.h PATHS ${ADDITIONAL_INCLUDE_DIRS})
-
-if (_tbb_include_dir)
-    # TODO: consider TBB_VERSION handling
-    set(_TBB_BUILD_MODES RELEASE DEBUG)
-    set(_TBB_DEBUG_SUFFIX _debug)
-
-    foreach (_tbb_component ${TBB_FIND_COMPONENTS})
-        if (NOT TARGET TBB::${_tbb_component})
-            add_library(TBB::${_tbb_component} SHARED IMPORTED)
-            set_property(TARGET TBB::${_tbb_component} APPEND PROPERTY INTERFACE_INCLUDE_DIRECTORIES ${_tbb_include_dir})
-
-            foreach(_TBB_BUILD_MODE ${_TBB_BUILD_MODES})
-                set(_tbb_component_lib_name ${_tbb_component}${_TBB_${_TBB_BUILD_MODE}_SUFFIX})
-                if (WIN32)
-                    find_library(${_tbb_component_lib_name}_lib ${_tbb_component_lib_name} PATHS ${ADDITIONAL_LIB_DIRS})
-                    find_file(${_tbb_component_lib_name}_dll ${_tbb_component_lib_name}.dll PATHS ${ADDITIONAL_LIB_DIRS})
-
-                    set_target_properties(TBB::${_tbb_component} PROPERTIES
-                                          IMPORTED_LOCATION_${_TBB_BUILD_MODE} "${${_tbb_component_lib_name}_dll}"
-                                          IMPORTED_IMPLIB_${_TBB_BUILD_MODE}   "${${_tbb_component_lib_name}_lib}"
-                                          )
-                else()
-                    find_library(${_tbb_component_lib_name}_so ${_tbb_component_lib_name} PATHS ${ADDITIONAL_LIB_DIRS})
-
-                    set_target_properties(TBB::${_tbb_component} PROPERTIES
-                                          IMPORTED_LOCATION_${_TBB_BUILD_MODE} "${${_tbb_component_lib_name}_so}"
-                                          )
-                endif()
-                if (${_tbb_component_lib_name}_lib AND ${_tbb_component_lib_name}_dll OR ${_tbb_component_lib_name}_so)
-                    set_property(TARGET TBB::${_tbb_component} APPEND PROPERTY IMPORTED_CONFIGURATIONS ${_TBB_BUILD_MODE})
-                    list(APPEND TBB_IMPORTED_TARGETS TBB::${_tbb_component})
-                    set(TBB_${_tbb_component}_FOUND 1)
-                endif()
-                unset(${_tbb_component_lib_name}_lib CACHE)
-                unset(${_tbb_component_lib_name}_dll CACHE)
-                unset(${_tbb_component_lib_name}_so CACHE)
-                unset(_tbb_component_lib_name)
-            endforeach()
-        endif()
-    endforeach()
-    unset(_TBB_BUILD_MODESS)
-    unset(_TBB_DEBUG_SUFFIX)
-endif()
-unset(_tbb_include_dir CACHE)
-
-list(REMOVE_DUPLICATES TBB_IMPORTED_TARGETS)
-
-find_package_handle_standard_args(TBB
-                                  REQUIRED_VARS TBB_IMPORTED_TARGETS
-                                  HANDLE_COMPONENTS)
